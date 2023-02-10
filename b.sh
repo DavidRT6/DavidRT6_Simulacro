55dc3e4abeff22e8df6180b69595563e21944895
David Roca <darota@floridauniversitaria.es>
2023-02-10 17:21:14 +0100

    Actualizando b.sh

--- b.sh
+++ b.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-maxLineas=`cat notas.txt | tail -n+2 | wc -l`
-let numMatriculas=0
-
-for i in `seq 1 $maxLineas`; do
-numMatriculas=$((numMatriculas+1))
-done
-
+numMatriculas=`cat notas.txt | tail -n+2 | wc -l`
 echo
 echo "El número de matrículas es: $numMatriculas"
